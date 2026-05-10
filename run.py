ed8e04c48d68eab2038b5376118a5167f1aca851
Suryansh24bsa10320 <suryansh24bsa10320@vitbhopal.ac.in>
2026-05-10 13:59:01 +0530

    Fix werkzeug production error

--- run.py
+++ run.py
@@ -1,6 +1,14 @@
+import os
 from app import create_app, socketio
 
 app = create_app()
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True, host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 10000))
+    socketio.run(
+        app,
+        debug=False,
+        host="0.0.0.0",
+        port=port,
+        allow_unsafe_werkzeug=True
+    )
